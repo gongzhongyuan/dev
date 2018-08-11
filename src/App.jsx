10220fd1f4ded21f35a45a64e50c00b828fa84f6
gongzhongyuan <192133440@qq.com>
2018-08-11 17:01:18 +0800

    dev

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import {Button} from "antd-mobile"
+
+export default function App() {
+    return <Button type="primary">HELLO REACT!!</Button>
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//入口文件
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+//渲染组件标签页面
+ReactDOM.render(<App/>,document.getElementById("root"))
